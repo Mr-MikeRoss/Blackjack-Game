f4a18e79abb77802e70d5d219604378995ef0d07
Mr-MikeRoss <MrMikeRoss1995@gmail.com>
2025-03-30 11:39:28 -0400

    Added BTN
    Added a "New Card" button as well as added a "btn-style" utility class.

--- index.js
+++ index.js
@@ -1,26 +1,36 @@
+//variables dom
 let messageEl = document.getElementById("message-el")
 let cardsEl = document.getElementById("cards-el")
 let sumEl = document.getElementById("sum-el")
+// game state
+let isAlive = false
+let hasBlackjack= false
+//elements of game
 let firstCard = 11
 let secondCard = 10
 let sum = firstCard + secondCard
-let hasBlackJack = false
-let isAlive = true
 let message = ""
-
+// When game starts
 function startGame() {
+    isAlive = true
     cardsEl.textContent = `Cards: ${firstCard}, ${secondCard}`
     sumEl.textContent = `Sum: ${sum}`
-
-    if (sum <= 20) {
-        message = "Do you want to draw a new card?"
+    if (sum < 21) {
+        message = "Would you like to draw another card?"
     } else if (sum === 21) {
-        message = "Wohoo! You've got Blackjack!"
-        hasBlackJack = true
+        message = "You've got Blackjack!"
+        hasBlackjack = true
+        console.log(hasBlackjack)
     } else {
-        message = "You're out of the game!"
+        message = "You're out of the game."
         isAlive = false
+        console.log(isAlive)
     }
-    messageEl.textContent = message  
+    messageEl.textContent = message
 }
+
+function newCard() {
+    console.log("uwu")
+}
+
 
