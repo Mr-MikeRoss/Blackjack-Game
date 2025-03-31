f0b231704c0e0e2cec4b4253419e0fb96502a13c
Mr-MikeRoss <MrMikeRoss1995@gmail.com>
2025-03-31 10:44:14 -0400

    Update index.js
    Practicing re-writing code by memory.

--- index.js
+++ index.js
@@ -1,43 +1,42 @@
-//variables dom
-let messageEl = document.getElementById("message-el")
-let cardsEl = document.getElementById("cards-el")
-let sumEl = document.getElementById("sum-el")
 // game state
 let isAlive = false
-let hasBlackjack= false
-//elements of game
-let firstCard = 11
+let hasBlackjack = false
+
+//DOM variables
+let cardsEl = document.getElementById("cards-el")
+let sumEl = document.getElementById("sum-el")
+let messageEl = document.getElementById("message-el")
+
+//global Variables
+let message = ""
+let firstCard = 10
 let secondCard = 6
 let sum = firstCard + secondCard
-let message = ""
-// When game starts
+
 function startGame() {
     renderGame()
 }
+
 function renderGame() {
     isAlive = true
     cardsEl.textContent = `Cards: ${firstCard}, ${secondCard}`
     sumEl.textContent = `Sum: ${sum}`
     if (sum < 21) {
-        message = "Would you like to draw another card?"
+        message = "Draw another card?"
     } else if (sum === 21) {
         message = "You've got Blackjack!"
         hasBlackjack = true
-        console.log(hasBlackjack)
     } else {
         message = "You're out of the game."
         isAlive = false
-        console.log(isAlive)
     }
     messageEl.textContent = message
 }
 
 function newCard() {
-    if(isAlive && hasBlackjack === false) {
-        let newCard = 10
+    if (isAlive && !hasBlackjack) {
+        let newCard = 7
         sum += newCard
         renderGame()
     } else {}
 }
-
-
